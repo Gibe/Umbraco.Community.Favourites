a3f7fb31bd1cf10c19ee2f48644f54a976b49c75
Luke Hook <lukehook@users.noreply.github.com>
2026-03-19 12:28:49 +0000

    Fix ctx undefined warning

--- src/Cork/Client/src/sidebars/pins.element.ts
+++ src/Cork/Client/src/sidebars/pins.element.ts
@@ -41,10 +41,12 @@ export class Pins extends UmbElementMixin(LitElement) {
 
     this.consumeContext(UMB_ACTION_EVENT_CONTEXT, (ctx) => {
       this._actionEventContext = ctx;
-      ctx.addEventListener(
-        UmbRequestReloadStructureForEntityEvent.TYPE,
-        this._boundRefresh,
-      );
+      if (ctx) {
+        ctx.addEventListener(
+          UmbRequestReloadStructureForEntityEvent.TYPE,
+          this._boundRefresh,
+        );
+      }
     });
   }
 
