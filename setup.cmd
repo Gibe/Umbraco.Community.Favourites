f868d6d695e10c12c82bd34c52b962cc092676cc
Luke Hook <lukehook@users.noreply.github.com>
2026-04-09 14:58:25 +0100

    Update setup.cmd for Umbraco Community Favourites

--- setup.cmd
+++ setup.cmd
@@ -5,23 +5,23 @@
 :: set up git
 git init
 git branch -M main
-git remote add origin https://github.com/g-dove/Cork.git
+git remote add origin https://github.com/Gibe/Umbraco.Community.Favourites
 
 :: ensure latest Umbraco templates used
 dotnet new install Umbraco.Templates --force
 
 :: use the umbraco-extension dotnet template to add the package project
 cd src
-dotnet new umbraco-extension -n "Cork" --site-domain "https://localhost:44373" --include-example
+dotnet new umbraco-extension -n "Umbraco.Community.Favourites" --site-domain "https://localhost:44373" --include-example
 
 :: replace package .csproj with the one from the template so has nuget info
 cd Cork
-del Cork.csproj
-ren Cork_nuget.csproj Cork.csproj
+del Umbraco.Community.Favourites.csproj
+ren Umbraco.Community.Favourites_nuget.csproj Umbraco.Community.Favourites.csproj
 
 :: add project to solution
 cd..
-dotnet sln add "Cork"
+dotnet sln add "Umbraco.Community.Favourites"
 
 :: add reference to project from test site
-dotnet add "Cork.TestSite/Cork.TestSite.csproj" reference "Cork/Cork.csproj"
+dotnet add "Umbraco.Community.Favourites.TestSite/Umbraco.Community.Favourites.TestSite.csproj" reference "Umbraco.Community.Favourites/Umbraco.Community.Favourites.csproj"
